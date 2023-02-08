df7c0aef6a4c7b5209a55d02da63923cd05baadb
Pablo del Barrio <pablodbb@gmail.com>
2023-02-08 12:45:38 +0100

    Almost finished first version with mockups, still to test responsiveness etc in production

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import React from "react";
 
 type Props = {};
@@ -5,18 +6,35 @@ type Props = {};
 const Projects = ({}: Props) => {
   const projects = [1, 2, 3, 4, 5];
   return (
-    <div className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0">
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0"
+    >
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         Projects
       </h3>
 
-      <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20">
+      <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
         {projects.map((project, i) => (
           <div
             key={i}
             className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen"
           >
-            <img
+            <motion.img
+              initial={{
+                y: -300,
+                opacity: 0,
+              }}
+              transition={{
+                duration: 1.2,
+              }}
+              whileInView={{
+                opacity: 1,
+                y: 0,
+              }}
+              viewport={{ once: true }}
               src="https://res.cloudinary.com/getoutbcn/image/upload/v1675715953/portfolio/projects/getout_j5jli1.png"
               alt="/"
             />
@@ -36,7 +54,7 @@ const Projects = ({}: Props) => {
       </div>
 
       <div className="w-full absolute top-[30%] bg-[#F7AB0A]/10 left-0 h-[500px] -skew-y-12"></div>
-    </div>
+    </motion.div>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,10 +6,12 @@ import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 import Head from "next/head";
+import Link from "next/link";
+import { HiOutlineChevronDoubleUp } from "react-icons/hi";
 
 export default function Home() {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
       <Head>
         <title>Pablo del Barrio | Frontend Developer</title>
       </Head>
@@ -39,6 +41,14 @@ export default function Home() {
       <section id="conract" className="snap-start">
         <Contact />
       </section>
+
+      <Link href="#hero">
+        <footer className="sticky bottom-5 w-full cursor-pointer">
+          <div className="flex items-center justify-center">
+            <HiOutlineChevronDoubleUp className=" filter hover:grayscale-0 cursor-pointer" />
+          </div>
+        </footer>
+      </Link>
     </div>
   );
 }

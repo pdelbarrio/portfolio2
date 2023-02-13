0ed19f013f9d77349d7f3b34e2690f743d62ed2f
Pablo del Barrio <pablodbb@gmail.com>
2023-02-13 17:58:26 +0100

    Updated slogan in navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -110,7 +110,9 @@ const Navbar = () => {
               </div>
             </div>
             <div className="border-b border-gray-300 my-4">
-              <p className="w-[85%] md:w-[90%] py-4">Never stop learning</p>
+              <p className="w-[85%] md:w-[90%] py-4">
+                Problem solver & fast learner
+              </p>
             </div>
           </div>
           <div className="py-4 flex flex-col">

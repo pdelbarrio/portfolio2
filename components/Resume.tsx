d2bf4e7d4694bdc12723a3b4be2c0ed5751f8f53
Pablo del Barrio <pablodbb@gmail.com>
2023-04-12 17:28:50 +0200

    updated resume

--- components/Resume.tsx
+++ components/Resume.tsx
@@ -16,7 +16,7 @@ function Resume({}) {
 
       <div className="flex flex-col md:flex-row justify-center items-center">
         <Link
-          href="https://res.cloudinary.com/getoutbcn/image/upload/v1679234183/portfolio/PablodelBarrio_english2023_rvsq38.pdf"
+          href="https://res.cloudinary.com/getoutbcn/image/upload/v1681313200/portfolio/PablodelBarrio_english2023_haeity.pdf"
           target="_blank"
         >
           <button className="text-black w-40 uppercase bg-white hover:scale-105 transition duration-200 focus_outline-none text-sm rounded-lg px-10 py-2.5 text-center font-bold mr-5 mb-5 md:mb-0">
@@ -25,7 +25,7 @@ function Resume({}) {
         </Link>
 
         <Link
-          href="https://res.cloudinary.com/getoutbcn/image/upload/v1679234183/portfolio/PablodelBarrio_spanish2023_wdd2y4.pdf"
+          href="https://res.cloudinary.com/getoutbcn/image/upload/v1681313200/portfolio/PablodelBarrio_spanish2023_khjrgw.pdf"
           target="_blank"
         >
           <button className="text-black w-40 uppercase bg-white hover:scale-105 transition duration-200 focus_outline-none text-sm rounded-lg px-10 py-2.5 text-center font-bold mr-5 mt-5 md:mt-0">

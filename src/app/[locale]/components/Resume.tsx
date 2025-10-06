0ea506b846784083fe35baf75c420b438b09cfd6
Pablo del Barrio <pablodbb@gmail.com>
2025-10-06 19:31:44 +0200

    update resume

--- src/app/[locale]/components/Resume.tsx
+++ src/app/[locale]/components/Resume.tsx
@@ -20,7 +20,7 @@ function Resume({}) {
 
       <div className="flex flex-col md:flex-row justify-center items-center">
         <Link
-          href="https://res.cloudinary.com/getoutbcn/image/upload/v1728998350/PablodelBarrio_Octubre2024ENG_taoho1.pdf"
+          href="https://res.cloudinary.com/getoutbcn/image/upload/v1759771701/PablodelBarrio_2025Eng_nqi87p.pdf"
           target="_blank"
         >
           <button className="text-black w-40 uppercase bg-white hover:scale-105 transition duration-200 focus_outline-none text-sm rounded-lg px-10 py-2.5 text-center font-bold mr-5 mb-5 md:mb-0">
@@ -29,7 +29,7 @@ function Resume({}) {
         </Link>
 
         <Link
-          href="https://res.cloudinary.com/getoutbcn/image/upload/v1728998312/PablodelBarrio_Octubre2024CAST_fte7ww.pdf"
+          href="https://res.cloudinary.com/getoutbcn/image/upload/v1759771701/PablodelBarrio_2025Cast_c6dhfh.pdf"
           target="_blank"
         >
           <button className="text-black w-40 uppercase bg-white hover:scale-105 transition duration-200 focus_outline-none text-sm rounded-lg px-10 py-2.5 text-center font-bold mr-5 mt-5 md:mt-0">

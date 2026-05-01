98703b0878afe7d64a0e57a021bc4dde3b8c679e
Pablo del Barrio <pablodbb@gmail.com>
2026-05-01 20:13:36 +0200

    Updated spanish cv

--- src/app/[locale]/components/Resume.tsx
+++ src/app/[locale]/components/Resume.tsx
@@ -19,17 +19,17 @@ function Resume({}) {
       </h3>
 
       <div className="flex flex-col md:flex-row justify-center items-center">
-        <Link
+        {/* <Link
           href="https://res.cloudinary.com/getoutbcn/image/upload/v1759771701/PablodelBarrio_2025Eng_nqi87p.pdf"
           target="_blank"
         >
           <button className="text-black w-40 uppercase bg-white hover:scale-105 transition duration-200 focus_outline-none text-sm rounded-lg px-10 py-2.5 text-center font-bold mr-5 mb-5 md:mb-0">
             {t("DownloadEng")}
           </button>
-        </Link>
+        </Link> */}
 
         <Link
-          href="https://res.cloudinary.com/getoutbcn/image/upload/v1759771701/PablodelBarrio_2025Cast_c6dhfh.pdf"
+          href="https://res.cloudinary.com/getoutbcn/image/upload/v1777659119/CVPablodelBarrio2026Castellano_bpiqbz.pdf"
           target="_blank"
         >
           <button className="text-black w-40 uppercase bg-white hover:scale-105 transition duration-200 focus_outline-none text-sm rounded-lg px-10 py-2.5 text-center font-bold mr-5 mt-5 md:mt-0">
